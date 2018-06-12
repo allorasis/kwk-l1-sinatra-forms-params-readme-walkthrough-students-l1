ae6044477f12086e94bf42c3baefce50b4d370fe
Allora Sisnroy <allsis@icloud.com>
2018-06-12 21:47:48 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get "/hello/:name" do
-    erb :
+  get "/" do
+    erb :food_form
   end
 
   # Add your post route and action below
